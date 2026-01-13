35bdbacb01b57f24b1dc6e1220303d23371f771e
exitingtheloop <christiangabriel.cote@gmail.com>
2026-01-13 10:57:25 -0800

    client: cart page + orders service

--- OrderUp.Client/Program.cs
+++ OrderUp.Client/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ApiClient>();
 builder.Services.AddScoped<MenuApi>();
+builder.Services.AddScoped<OrdersApi>();
 builder.Services.AddScoped<CartState>();
 
 await builder.Build().RunAsync();
